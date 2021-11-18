8cb9b93348f7c07169371078387d59db43605782
yasuiyasumasa <a203429@yahoo.co.jp>
2021-11-18 15:43:28 +0000

    2021/11/18

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" contet="IE=edge">
+    <meta name="viewport" contet="width=device-width" initial-scale=1>
+    
+    <title>MyNews</title>
+  </head>
+  <body>
+    <h1>myプロフィール作成画面</h1>
+  </body>
+</html>
+  
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        
+        <!-- CSRF Token -->
+          {{-- 後の章で説明します --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます --}}
+        <title>@yield('title')</title>
+        </-->
